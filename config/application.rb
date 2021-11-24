c5478c38924d7a9921d59a61a7f1b1475a77207e
pigchevalier <nadine.boudkina@gmail.com>
2021-11-24 23:59:56 +0300

    first_task

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module TestGuru
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Europe/Moscow"
+    config.i18n.available_locales = [:en, :ru]
   end
 end
