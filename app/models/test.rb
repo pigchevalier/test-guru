3fd81d5e4604b813e57233e4a9168c5e6c5ab7bb
pigchevalier <nadine.boudkina@gmail.com>
2021-11-29 12:55:57 +0300

    added joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
   def self.title_by_category(category_name)
-    id_array = Category.where("title = ?", category_name).pluck(:id)
-    tests_titles = []
-    id_array.each do |id|
-      tests_titles.push(self.where("category_id = ?", id).order(title: :desc).pluck(:title))
-    end 
-    tests_titles
+    #id_array = Category.where("title = ?", category_name).pluck(:id)
+    #tests_titles = []
+    #id_array.each do |id|
+    #  tests_titles.push(self.where("category_id = ?", id).order(title: :desc).pluck(:title))
+    #end 
+    tests_titles = Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category_name).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    user_tests = Test.where("level = ? AND author_id = ?", level, self.id)
+    user_tests = Test.joins("INNER JOIN users ON tests.author_id = users.id").where(author_id: self.id, level: level) 
   end
 end
