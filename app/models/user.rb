45dd3c4893c07a236c59ec7c059bce1671b2ae04
pigchevalier <nadine.boudkina@gmail.com>
2021-11-29 22:40:32 +0300

    user joins fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins("INNER JOIN results ON tests.id = results.test_id INNER JOIN users ON results.user_id = users.id").where(users: { id: self.id}, level: level) 
+    Test.joins("INNER JOIN results ON tests.id = results.test_id ").where(results: { user_id: self.id}, level: level) 
   end
 end
