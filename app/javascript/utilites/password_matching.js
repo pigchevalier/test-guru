65b68f22f7d374ce1bf132cf329d651f734ccf19
pigchevalier <nadine.boudkina@gmail.com>
2021-12-12 17:21:27 +0300

    fixes

--- app/javascript/utilites/password_matching.js
+++ app/javascript/utilites/password_matching.js
@@ -1,18 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('#user\\_password\\_confirmation')
+  let control = document.querySelector('#user_password_confirmation')
   if(control) { control.addEventListener('input', passwordMatching) }
 })
 
 function passwordMatching(){
-  var password = document.querySelector('#user\\_password')
-  var password_confirmation = document.querySelector('#user\\_password\\_confirmation')
+  let password = document.querySelector('#user_password')
+  let password_confirmation = document.querySelector('#user_password_confirmation')
   if (password.value != password_confirmation.value && password_confirmation.value != ''){
     document.querySelector('.octicon-alert').classList.remove('hide')
   } else {
     document.querySelector('.octicon-alert').classList.add('hide')
   }
-
 }
-
-
-
